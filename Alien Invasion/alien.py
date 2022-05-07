c24ff0f2b9834c5dcb8b71bf308825046763795b
Elseter <104609977+Elseter@users.noreply.github.com>
2022-05-07 00:30:18 -0400

    5/7/2022 Update

--- Alien Invasion/alien.py
+++ Alien Invasion/alien.py
@@ -7,9 +7,10 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         #load the alien image and set its rect attribute
-        self.image = pygame.image.load('Alien Invasion/images/alien')
+        self.image = pygame.image.load('Alien Invasion/images/alien.bmp')
         self.rect = self.image.get_rect()
 
         #Start each new alien near the top left of the screen
@@ -18,3 +19,13 @@ class Alien(Sprite):
 
         #Store the alien's exact horzontal position
         self.x = float(self.rect.x)
+    
+    def check_edges(self):
+        """Return True if alein is at edge of screen"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <=0:
+            return True
+    
+    def update(self):
+        self.x += (self.settings.alien_speed* self.settings.fleet_direction)
+        self.rect.x = self.x
--- Alien Invasion/alien_invasion.py
+++ Alien Invasion/alien_invasion.py
@@ -23,7 +23,7 @@ class AlienInvasion:
         self.bullet_firing = False
         self.bullets = pygame.sprite.Group()
 
-        # Variables from hell that get the hold to fire working
+        # Variables from hell that get 'hold to fire' working
         self.first_press = 0
         self.first_fire = True
         self.previous = 10000
@@ -104,11 +104,50 @@ class AlienInvasion:
     #---------------------------------------------------------------------------------------
     # Aliens Handling
 
+    def _create_alien(self, alien_number, row_number):
+            alien = Alien(self)
+            alien_width, alien_height = alien.rect.size
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+            self.aliens.add(alien)
+
     def _create_fleet(self):
         """Create a fleet of aliens"""
-        # make an alien
+        # determine how many fit in a row
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_of_aliens = available_space_x // (2 * alien_width)
+
+        #Determine how many rows fit
+        ship_height = self.ship.rect.height
+        available_space_y = self.settings.screen_height - (3* alien_height) - ship_height
+        number_rows = available_space_y // (2 * alien_height)
+
+        #Create Full Fleet
+        for row_number in range(number_rows):
+            for alien_number in range(number_of_aliens):
+                self._create_alien(alien_number, row_number)
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        """ Drop the entire Fleet and change direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+        
+    def _update_aliens(self):
+        """ Update the positions of all the aliens in the fleet"""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+
 
     #---------------------------------------------------------------------------------------
 
@@ -128,6 +167,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update(self)
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
 

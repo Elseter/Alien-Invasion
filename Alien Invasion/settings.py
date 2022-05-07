9f35ac671d90aa0ac1e862a17d8ba13b3c9e2cb8
Elseter <rking3425@gmail.com>
2022-05-07 13:58:05 -0400

    5/7/2022 Update 3

--- Alien Invasion/settings.py
+++ Alien Invasion/settings.py
@@ -3,22 +3,26 @@ class Settings:
 
     def __init__(self):
         """ Initialize the game's settings"""
+
+#---------------------------------------------------------------------------------------
+# Windows 2056x1080 Settings (active by default)
+
         # Screen Settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
         #Ship Settings
-        self.ship_speed = 1
+        self.ship_speed = 1.0      # default = 1.0 (Mac = 7.0)
 
         #Bullet Settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.0      # default = 1.0 (Mac = 10)
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullet_cooldown = 300
+        self.bullet_cooldown = 300     # default 300 (Mac = 500)
 
         #Alien settings
-        self.alien_speed = 0.2
-        self.fleet_drop_speed = 5
+        self.alien_speed = 0.2        # default = 0.2 (Mac = 1)
+        self.fleet_drop_speed = 5      # default = 5 (Mac = 5)
         self. fleet_direction = 1 # 1 = Right, -1 = left

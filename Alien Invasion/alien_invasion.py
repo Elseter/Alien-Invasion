2de0ea74b7edd27245b050ed04e62cc9ef9c1756
Elseter <104609977+Elseter@users.noreply.github.com>
2022-05-16 16:16:16 -0400

    5/16 Update 3

--- Alien Invasion/alien_invasion.py
+++ Alien Invasion/alien_invasion.py
@@ -122,21 +122,6 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
     
-    def _ship_hit(self):
-        """Respond to the ship being hit by an alien"""
-        self.stats.ships_left -= 1
-
-        #Remove aliens and bullets
-        self.aliens.empty()
-        self.bullets.empty()
-
-        #Create a new fleet and center the ship
-        self._create_fleet()
-        self.ship.center_ship()
-
-        #Pause in gameplay
-        sleep(0.5)
-
     
     #---------------------------------------------------------------------------------------
     # Aliens Handling
@@ -187,6 +172,40 @@ class AlienInvasion:
         #Check for alien-ship collisions
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
+        
+        # Look for aliens hitting the bottom of the screen
+        self._check_aliens_bottom()
+
+    #---------------------------------------------------------------------------------------
+    #Game Win/Loss Conditions
+
+    def _ship_hit(self):
+        """Respond to the ship being hit by an alien"""
+        if self.stats.ships_left > 0:
+            self.stats.ships_left -= 1
+
+            #Remove aliens and bullets
+            self.aliens.empty()
+            self.bullets.empty()
+
+            #Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
+
+            #Pause in gameplay
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
+    
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen"""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                #Treat this the same as being hit
+                self._ship_hit()
+                break
+
 
     #---------------------------------------------------------------------------------------
 
@@ -204,9 +223,11 @@ class AlienInvasion:
         while True:
             # Constatly running events
             self._check_events()
-            self.ship.update(self)
-            self._update_bullets()
-            self._update_aliens()
+            if self.stats.game_active:
+                self.ship.update(self)
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
 
